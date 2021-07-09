4c54e328a6902fb46699b24fa0c7c010f8da60a0
lucas-leoni <lucas.leoni09@gmail.com>
2021-07-09 17:42:33 -0300

    CRUD funcionando

--- index.js
+++ index.js
@@ -19,34 +19,17 @@ connection
 
 const Produto = require('./database/Produto');
 
-app.get('/cadastrar-produto', (req, res) => {
-    
-    res.render('cadastrar-produto');
-});
-
-app.post('/salvar-produto', (req, res) => {
-    let nm_produto = req.body.nm_produto;
-    let ds_produto = req.body.ds_produto;
-    let vl_unitario = req.body.vl_unitario;
-    let categoria = req.body.categoria;
-
-    Produto.create({
-      nm_produto: nm_produto, 
-      ds_produto: ds_produto,
-      vl_unitario: vl_unitario,
-      categoria: categoria
-    }).then(() => {
-      res.redirect('/');
-    }).catch((error) => {
-      console.error(`Ocorreu um erro, ao salvar o produto -  ${error}`);
-    });
-});
-
 app.get('/', (req, res) => {
+    const { Op } = require("sequelize");
     Produto.findAll({
       raw: true, 
-      order: [[ 'id' ]],
-      where: {categoria: 'pizza'}
+      order: [[ 'id', 'DESC' ]],
+      where: {
+        [Op.or]: [
+          { categoria: 'pizza' },
+          { categoria: 'pizzas' }
+        ]
+      }
     }).then((produto) => {
       res.render('index', {
         produto: produto
@@ -55,10 +38,16 @@ app.get('/', (req, res) => {
 });
 
 app.get('/pizzas', (req, res) => {
+    const { Op } = require("sequelize");
     Produto.findAll({
       raw: true, 
-      order: [[ 'id' ]],
-      where: {categoria: 'pizza'}
+      order: [[ 'id', 'DESC' ]],
+      where: {
+        [Op.or]: [
+          { categoria: 'pizza' },
+          { categoria: 'pizzas' }
+        ]
+      }
     }).then((produto) => {
       res.render('index', {
         produto: produto
@@ -67,10 +56,16 @@ app.get('/pizzas', (req, res) => {
 });
 
 app.get('/massas', (req, res) => {
+    const { Op } = require("sequelize");
     Produto.findAll({
       raw: true, 
-      order: [[ 'id' ]],
-      where: {categoria: 'massa'}
+      order: [[ 'id', 'DESC' ]],
+      where: {
+        [Op.or]: [
+          { categoria: 'massa' },
+          { categoria: 'massas' }
+        ]
+      }
     }).then((produto) => {
       res.render('index', {
         produto: produto
@@ -79,10 +74,16 @@ app.get('/massas', (req, res) => {
 });
 
 app.get('/calzones', (req, res) => {
+    const { Op } = require("sequelize");
     Produto.findAll({
       raw: true, 
-      order: [[ 'id' ]],
-      where: {categoria: 'calzone'}
+      order: [[ 'id', 'DESC' ]],
+      where: {
+        [Op.or]: [
+          { categoria: 'calzone' },
+          { categoria: 'calzones' }
+        ]
+      }
     }).then((produto) => {
       res.render('index', {
         produto: produto
@@ -91,10 +92,16 @@ app.get('/calzones', (req, res) => {
 });
 
 app.get('/esfihas', (req, res) => {
+    const { Op } = require("sequelize");
     Produto.findAll({
       raw: true, 
-      order: [[ 'id' ]],
-      where: {categoria: 'esfiha'}
+      order: [[ 'id', 'DESC' ]],
+      where: {
+        [Op.or]: [
+          { categoria: 'esfiha' },
+          { categoria: 'esfihas' }
+        ]
+      }
     }).then((produto) => {
       res.render('index', {
         produto: produto
@@ -103,10 +110,16 @@ app.get('/esfihas', (req, res) => {
 });
 
 app.get('/sobremesas', (req, res) => {
+    const { Op } = require("sequelize");
     Produto.findAll({
       raw: true, 
-      order: [[ 'id' ]],
-      where: {categoria: 'sobremesa'}
+      order: [[ 'id', 'DESC' ]],
+      where: {
+        [Op.or]: [
+          { categoria: 'sobremesa' },
+          { categoria: 'sobremesas' }
+        ]
+      }
     }).then((produto) => {
       res.render('index', {
         produto: produto
@@ -115,15 +128,44 @@ app.get('/sobremesas', (req, res) => {
 });
 
 app.get('/bebidas', (req, res) => {
+    const { Op } = require("sequelize");
     Produto.findAll({
       raw: true, 
-      order: [[ 'id' ]],
-      where: {categoria: 'bebida'}
+      order: [[ 'id', 'DESC' ]],
+      where: {
+        [Op.or]: [
+          { categoria: 'bebida' },
+          { categoria: 'bebidas' }
+        ]
+      }
     }).then((produto) => {
       res.render('index', {
         produto: produto
       });
     });
+});
+
+app.get('/cadastrar-produto', (req, res) => {
+    
+  res.render('cadastrar-produto');
+});
+
+app.post('/salvar-produto', (req, res) => {
+  let nm_produto = req.body.nm_produto;
+  let ds_produto = req.body.ds_produto;
+  let vl_unitario = req.body.vl_unitario;
+  let categoria = req.body.categoria;
+
+  Produto.create({
+    nm_produto: nm_produto, 
+    ds_produto: ds_produto,
+    vl_unitario: vl_unitario,
+    categoria: categoria
+  }).then(() => {
+    res.redirect('/');
+  }).catch((error) => {
+    console.error(`Ocorreu um erro ao salvar o produto -  ${error}`);
+  });
 });
 
 app.get('/produto/:id', (req, res) => {
@@ -136,6 +178,51 @@ app.get('/produto/:id', (req, res) => {
         produto: produto
       });
     });
+});
+
+app.get('/produto/:id/atualizar', (req, res) => {
+  const {id} = req.params;
+  Produto.findAll({
+    raw: true, 
+    where: {id: id}
+  }).then((produto) => {
+    res.render('atualizar-produto', {
+      produto: produto
+    });
+  });
+});
+
+app.post('/atualizar-produto/:id', (req, res) => {
+  const id = req.params.id;
+  let att_nm_produto = req.body.att_nm_produto;
+  let att_ds_produto = req.body.att_ds_produto;
+  let att_vl_unitario = req.body.att_vl_unitario;
+  let att_categoria = req.body.att_categoria;
+  console.log(`id: ${id}, attValor: ${att_vl_unitario}`);
+  Produto.update({
+    nm_produto: att_nm_produto, 
+    ds_produto: att_ds_produto,
+    vl_unitario: att_vl_unitario,
+    categoria: att_categoria
+  }, {
+    where: {id: id}
+  }).then(() => {
+    res.redirect('/');
+  }).catch((error) => {
+    console.error(`Ocorreu um erro ao atualizar o produto -  ${error}`);
+  });
+});
+
+app.get('/deletar-produto/:id', (req, res) => {
+  const id = req.params.id;
+  Produto.destroy({ 
+    where: { id : id } 
+  }).then(() => {
+    console.log('removi o registro');
+    res.redirect('/');
+  }).catch((error) => {
+    console.log('errorrrr', error);
+  });
 });
 
 app.listen(9000, (erro) => {
